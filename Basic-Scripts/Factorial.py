94dc45dc29e810726b3334ec1701d10a79c51c51
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-01-02 22:00:48 +0530

    updated and improved the code of finding factorial

--- Basic-Scripts/Factorial.py
+++ Basic-Scripts/Factorial.py
@@ -1,19 +1,15 @@
 # Python program to find the factorial of a number provided by the user.
 
-# change the value for a different result
-num = 7
 
-# uncomment to take input from the user
-#num = int(input("Enter a number: "))
+# we are finding the factorial of a number by recursion
+def factorial(n):
+   if(n < 0):
+      print("Sorry, factorial does not exist for negative numbers")
+   elif(n <= 1):
+      return 1
 
-factorial = 1
+   return n * factorial(n-1)
 
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+if __name__ == '__main__':
+   num = int(input("Enter a number: "))
+   print(f"The factorial of {num} is {factorial(num)} !")
